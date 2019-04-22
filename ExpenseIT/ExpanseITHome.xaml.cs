b9a7d40110ced26af720d664590fe04fa6cb6159
Maciej Szymczyk <czajnero@gmail.com>
2019-04-22 03:32:16 +0200

    Initial commit

---
+++ ExpenseIT/ExpanseITHome.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExpenseIT
+{
+    /// <summary>
+    /// Logika interakcji dla klasy ExpanseITHome.xaml
+    /// </summary>
+    public partial class ExpanseITHome : Page
+    {
+        public ExpanseITHome()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ExpenseReportPage expenseReportPage = new ExpenseReportPage();
+            this.NavigationService.Navigate(expenseReportPage);
+        }
+    }
+}
